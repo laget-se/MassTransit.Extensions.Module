58b9ef919f8d5462c5f791d1b319e5e1ad9ce519
Erik Hennerfors <erik.hennerfors@laget.se>
2023-11-24 11:34:46 +0100

    Update(s)

--- MassTransit.Extensions.Module/Module.cs
+++ MassTransit.Extensions.Module/Module.cs
@@ -1,15 +1,7 @@
-﻿using System;
-
-namespace MassTransit.Extensions.Module
+﻿namespace MassTransit.Extensions.Module
 {
     public abstract class Module
     {
-        public void Configure(IBusRegistrationConfigurator configurator)
-        {
-            if (configurator == null)
-            {
-                throw new ArgumentNullException(nameof(configurator));
-            }
-        }
+        protected abstract void Configure(IBusRegistrationConfigurator configurator);
     }
 }
